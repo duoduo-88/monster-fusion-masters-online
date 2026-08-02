58e5dcb276d054ac5586c32230ff671e2dc64f09
duoduo-88 <duoduo-88@users.noreply.github.com>
2026-08-03 01:50:11 +0800

    Improve game typography and scrollbars

--- online/scripts/build.mjs
+++ online/scripts/build.mjs
@@ -30,6 +30,7 @@ await build({
 let gameHtml = await readFile(localGame, "utf8");
 gameHtml = gameHtml
   .replaceAll("../online/public/fonts/VT323-Regular.ttf", "./fonts/VT323-Regular.ttf")
+  .replaceAll("../online/public/retro-scrollbar.css", "./retro-scrollbar.css")
   .replace("</head>", '<link rel="stylesheet" href="./online-game.css"></head>')
   .replace("</body>", '<script src="./config.js"></script><script type="module" src="./game-online.js"></script></body>');
 await writeFile(resolve(dist, "game.html"), gameHtml, "utf8");
